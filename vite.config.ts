ba02467355c8263bb9e4ca07b0d3ce96b75bfb77
Harvey Dela Cruz <harvey.delacruz@connectos.co>
2024-03-19 14:33:50 +0800

    updated build and npm publish configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "DuxeComponents",
+      fileName: (format) => `index.${format}.js`
+    },
     rollupOptions: {
+      external: ["react", "react-dom"],
       output: {
-        entryFileNames: 'index.js',
-        assetFileNames: 'styles.css'
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
       }
-    }
+    },
+    sourcemap: true,
+    emptyOutDir: true,
   }
 })
